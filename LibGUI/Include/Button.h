421c1b937e9f244fd4cc4f1e446ce620a6036288
xudh <xudh@xudh.me>
2017-12-24 11:07:09 +0800

    add LibGID_FB LibGUI directory

---
+++ LibGUI/Include/Button.h
@@ -0,0 +1,11 @@
+#ifndef Button___H
+#define Button___H
+
+struct Button
+{
+	char name[32];
+};
+
+struct Button *CreateButton(void);
+#endif
+
---
+++ LibGUI/Source/Button.c
@@ -0,0 +1,10 @@
+#ifndef Button___H
+#define Button___H
+
+#include "Button.h"
+
+struct Button *CreateButton(void)
+{
+	return NULL;
+}
+
